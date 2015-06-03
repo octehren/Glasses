75bc3490f7557e644acb25fe0aedf7e5dd022d60
otamm <oivatom@gmail.com>
2015-06-03 04:00:44 -0300

    Added full-search

--- lib/glasses.rb
+++ lib/glasses.rb
@@ -24,6 +24,28 @@ module Glasses
     end
   end
 
+  def self.full_search(model_to_search,params_hash)
+    query = ""
+    query_params = []
+    params_hash.each do |key,val|
+      if !val.empty?
+        if key.to_s[key.size-3,key.size] == "_id"
+          query += "#{key} = ? AND "
+          query_params.push(val.to_s)
+        else
+          query += "#{key} LIKE ? AND " # percent sign matches any string of 0 or more chars.
+          query_params.push("%" + val + "%")
+        end
+      end
+    end
+    if !query.empty?
+      query = query[0,query.size-5]
+      model_to_search.where(query, query_params)
+    else
+      []
+    end
+  end
+
     def self.search_range(model_to_search,params_hash)
     query = ""
     query_params = []
@@ -42,6 +64,35 @@ module Glasses
         else
           query += "#{key} LIKE ? AND " # percent sign matches any string of 0 or more chars.
           query_params.push(val + "%")
+        end
+      end
+    end
+    if !query.empty?
+      query = query[0,query.size-5]
+      model_to_search.where(query, query_params)
+    else
+      []
+    end
+  end
+
+  def self.full_search_range(model_to_search,params_hash)
+    query = ""
+    query_params = []
+    params_hash.each do |key,val|
+      if !val.empty?
+        key_suffix = key.to_s[key.size-3,key.size]
+        if key_suffix == "min"
+          query += "#{key[0,key.size-4]} >= ? AND "
+          query_params.push(val.to_s)
+        elsif key_suffix == "max"
+          query += "#{key[0,key.size-4]} <= ? AND "
+          query_params.push(val.to_s)
+        elsif key_suffix == "_id"
+          query += "#{key} = ? AND "
+          query_params.push(val.to_s)
+        else
+          query += "#{key} LIKE ? AND " # percent sign matches any string of 0 or more chars.
+          query_params.push("%" + val + "%")
         end
       end
     end
--- spec/tests/range_search_test.rb
+++ spec/tests/range_search_test.rb
@@ -1,11 +1,15 @@
 describe 'search entries which have a value within a specific range' do
 	
 	it 'returns all the results with a field bigger than or equal to some arbitrary value' do
-		(Glasses.search_range(Player,{age_min: '110'}).size).should == 3
+		(Glasses.search_range(Player, {age_min: '110'}).size).should == 3
 	end
 
 	it 'returns all the results with a field lesser than or equal to some arbitrary value' do
-		(Glasses.search_range(Player,{age_max: '110'}).size).should == 8
+		(Glasses.search_range(Player, {age_max: '110'}).size).should == 8
+	end
+
+	it 'returns all the results with a field bigger than or equal to some arbitrary value and with another field lesser than or equal to some other arbitrary value' do
+		(Glasses.search_range(Player, {age_min: '20', age_max: '110'}).size).should == 4
 	end
 
 end
